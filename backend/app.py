da3c6284f5fd124ef90cba6574116251bc5f7100
DESKTOP-GID09QK\yoshi <hiro.engineer0690@gmail.com>
2022-11-19 14:49:42 +0900

    [update] フロントとバックの連携完了

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from random import *
 
 # static_folder：vueでビルドした静的ファイルのパスを指定
 # template_folder：vueでビルドしたindex.htmlのパスを指定
-app = Flask(__name__, static_folder = "./../frontend/dist/static", template_folder="./../frontend/dist")
+app = Flask(__name__, static_folder="../frontend/dist/static", template_folder="../frontend/dist")
 
 app.config.from_object(__name__)
 
@@ -17,7 +17,7 @@ CORS(app)
 # 任意のリクエストを受け取った時、index.htmlを参照
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def catch(path):
+def index(path):
     return render_template("index.html")
 
 # '/rand'が叩かれた時、乱数を生成
@@ -30,4 +30,4 @@ def random():
 
 # app.run(host, port)：hostとportを指定してflaskサーバを起動
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', debug=True)
