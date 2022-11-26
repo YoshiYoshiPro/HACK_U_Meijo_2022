6d6c31fd30ef0b6b5f1af1997206ac3b2690e80f
DESKTOP-GID09QK\yoshi <hiro.engineer0690@gmail.com>
2022-11-26 22:26:09 +0900

    [update] ユーザー画面追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,16 +5,43 @@
       <v-toolbar-title>OTASUKE</v-toolbar-title>
       <v-space></v-space>
       <v-toolbar-items>
-        <v-btn text>投稿</v-btn>
+        <v-btn v-on="on" text>投稿</v-btn>
       </v-toolbar-items>
     </v-app-bar>
+
     <v-navigation-drawer app v-model="drawer" clipped>
-      Navigation Lists
+      <template v-slot:prepend>
+        <v-list-item
+          lines="two"
+          prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg"
+          title="Jane Smith"
+          subtitle="Logged in"
+        ></v-list-item>
+      </template>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item
+          prepend-icon="mdi-home-city"
+          title="Home"
+          value="home"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account"
+          title="My Account"
+          value="account"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account-group-outline"
+          title="Users"
+          value="users"
+        ></v-list-item>
+      </v-list>
     </v-navigation-drawer>
     <!-- <v-main>
       <router-view />
     </v-main> -->
-    <v-footer color="primary" dark app>OTASUKE</v-footer>
   </v-app>
 </template>
 
@@ -24,6 +51,11 @@ export default {
 
   data() {
     return {
+      items: [
+        { title: "Home", icon: "mdi-home-city" },
+        { title: "My Account", icon: "mdi-account" },
+        { title: "Users", icon: "mdi-account-group-outline" },
+      ],
       drawer: null,
     };
   },
