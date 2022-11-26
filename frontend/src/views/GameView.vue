19464c602172873dc038b39eb30589a489462acd
neruneruna7 <113534341+neruneruna7@users.noreply.github.com>
2022-11-26 23:03:12 +0900

    [add]Game画面を追加　

---
+++ frontend/src/views/GameView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div SeachNumber>
+    <GameSearchNumVue />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import GameBoard from "@/components/GameBoard.vue";
+import GameSearchNumVue from "@/components/GameSearchNum.vue";
+export default {
+  name: "GameView",
+  components: {
+    //GameBoard,
+    GameSearchNumVue,
+  },
+};
+</script>
+
+<style scoped>
+.mb-20 {
+  margin-bottom: 20px;
+}
+.TicTacToe {
+  border: 2px solid #333;
+  background-color: #ddd;
+  margin-left: 20%;
+  margin-right: 20%;
+  text-align: center;
+  padding: 10px;
+}
+</style>
