160298cbc8af7b5d0c538ee7f596a0879940f0ef
DESKTOP-GID09QK\yoshi <hiro.engineer0690@gmail.com>
2022-11-30 21:38:35 +0900

    [update] firstcommit-1error

---
+++ frontend/src/components/index-login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="links">
+    <a @click="signIn" class="button--green">signIn</a>
+  </div>
+</template>
+
+<script>
+// script タグ内を抜粋
+import firebase from "firebase";
+
+export default {
+  methods: {
+    signIn: function () {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+    },
+  },
+};
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,24 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 
+/* import Firebase */
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2nu7GuWecfS2YVlTO1fa4QgDKG-NjDmk",
+  authDomain: "otasuke-f5bf7.firebaseapp.com",
+  projectId: "otasuke-f5bf7",
+  storageBucket: "otasuke-f5bf7.appspot.com",
+  messagingSenderId: "92203841168",
+  appId: "1:92203841168:web:773cf9ccb8ce2cc2b74a15",
+  measurementId: "G-R2Q36QYYPK",
+};
+firebase.initializeApp(firebaseConfig);
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount("#app");
+
 loadFonts();
 
 createApp(App).use(router).use(store).use(vuetify).mount("#app");
